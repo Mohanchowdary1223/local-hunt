e593177190299e9866012c2315bf72d07f3d44bd
Rayudu Veera Venkata Swamy <swamyrayudu7288@gmail.com>
2025-09-02 10:23:08 +0530

    seller page register setp1 to step4 completed

--- client/src/app/seller/layout.tsx
+++ client/src/app/seller/layout.tsx
@@ -1,11 +1,23 @@
+"use client";
 import SellerHeader from "@/components/layouts/SellerHeader";
+import { usePathname } from "next/navigation";
 
-
-export default function UserLayout({ children } : {children : React.ReactNode}) {
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const hideHeader =
+    pathname === "/seller/auth/login" ||
+    pathname === "/seller/auth/register/step1" ||
+    pathname === "/seller/auth/register/step2" ||
+    pathname === "/seller/auth/register/step3" ||
+    pathname === "/seller/auth/register/step4";
   return (
     <>
-      <SellerHeader/>
+      {!hideHeader && <SellerHeader />}
       {children}
     </>
-  )
+  );
 }
